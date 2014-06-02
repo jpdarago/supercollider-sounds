6b73ad1a17de17ba350d2ac53d587c4a2ac3dd5d
Juan Pablo Darago <jpdarago@gmail.com>
2014-06-02 03:14:03 -0300

    First visualization built. Gotta refactor and improve performance of code. Need special effects

--- tiling-visualization/client.js
+++ tiling-visualization/client.js
@@ -8,47 +8,83 @@
             ctx.clearRect(0, 0, canvas.width, canvas.height);
         }
 
-        var rectSpec = {
-            10: { 
-                time: 40,
-                scale: 0.4,
-                color: [255,0,0],
-            }
-        };
+        function randomColorWithAlpha(alpha){
+            return [
+                _.random(0,255),
+                _.random(0,255),
+                _.random(0,255),
+                alpha
+            ];
+        }
+
+        var rectSpec = {};
         var defaultRect = {
             scale: 0.0,
-            color: [255,255,255]
+            color: [255,255,255,1],
+            remaining: 0,
+            duration: 1
+        }
+
+
+        var socket = new WebSocket('ws://127.0.0.1:8080');
+        socket.onmessage = function(msg){
+            var data = JSON.parse(msg.data);
+            var note = data.note - 40;
+            var duration = data.duration - 30;
+
+            rectSpec[note] = {
+                scale: 0.4,
+                color: randomColorWithAlpha(1.0),
+                duration: 2*duration,
+                remaining: 2*duration
+            }
         }
 
         function updateRectangles(diff){
-
+            console.log(diff);
+            _.each(rectSpec, function(e,key){
+                e.remaining -= diff;
+                if(e.remaining < 0){
+                    e.remaining = 0;
+                }
+            });
+        }
+            
+        function applyAlphaFactor(color,frac){
+            return [color[0],color[1],color[2],color[3]*frac*3.0];
         }
 
         function buildRect(x,y,rectw,recth,count){
             var spec = rectSpec[count] || defaultRect;
             var scale = spec.scale, color = spec.color;
+
+            var frac = spec.remaining / spec.duration;
+
+            scale *= frac;
+            color = applyAlphaFactor(color, frac);
+
             return {
                 x: x-(scale/2)*rectw,
                 y: y-(scale/2)*recth,
                 width: (1+scale)*rectw,
                 height: (1+scale)*recth,
-                style: "rgb(" + color.join(",") + ")"
+                style: "rgba(" + color.join(",") + ")"
             }
         }
 
         function rectanglePositions(rectw,recth){
             var rects = [];
             for(var i = 0, count = 0;;i++){
-                if((i+1)*rectw > canvas.width){
+                if((i+1)*recth > canvas.height){
                     break;
                 }
                 for(var j = 0;; j++){
-                    if((j+1)*recth > canvas.height){
+                    if((j+1)*rectw > canvas.width){
                         break;
                     }
                     rects.push({
-                        x: i*rectw,
-                        y: j*recth,
+                        x: j*rectw,
+                        y: i*recth,
                         index: count
                     });
                     count++;
@@ -74,24 +110,24 @@
         }
 
         function diff(timer){
-            var time = timer();
+            var time = window.performance.now();
             return function(){
-                var newTime = timer();
+                var newTime = window.performance.now();
                 var prevTime = time;
                 time = newTime;
                 return newTime-prevTime;
             }
         }
 
-        var timeDiff = diff(window.performance.now);
+        var timeDiff = diff();
         function render(){
             requestAnimationFrame(render);
 
             var diff = timeDiff();
-            updateAllRectangles(diff);
+            updateRectangles(diff);
 
             clearCanvas();
-            drawRectangles(80,80);
+            drawRectangles(100,100);
         }
 
         render();
